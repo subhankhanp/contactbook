517e4abb4db3056782d3cc01421ef8c3fa77d6a1
subhankhan.p <subhankhan.p@bitlasoft.com>
2018-04-10 15:05:43 +0530

    production ip change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-server '54.152.121.248', user: 'deploy', roles: %w{web app db}
+server '52.90.237.194', user: 'deploy', roles: %w{web app db}
 
 
 # Configuration
